f9fda78986ccd38ce49a692390c403a6ca2af782
Satish <reachsatish@gmail.com>
2021-04-22 04:58:58 -0400

    added depends on

--- first_instance/main.tf
+++ first_instance/main.tf
@@ -19,6 +19,35 @@ resource "google_compute_instance" "appserver" {
 
 }
 
+
+resource "google_compute_instance" "second" {
+  name = "second-server"
+  machine_type = var.machine_type["dev"]
+
+  boot_disk {
+    initialize_params {
+      image= var.image
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  service_account {
+      scopes = [ "userinfo-email", "compute-ro", "storage-ro" ]
+  }
+
+  depends_on = [ google_compute_instance.appserver ]
+}
+
+
+# app server
 output "machine_type" { value = "${google_compute_instance.appserver.*.machine_type}" }
 output "name" { value = "${google_compute_instance.appserver.*.name}" }
 output "instance_id" {value = "${join(",", google_compute_instance.appserver.*.machine_type)}"}
+
+# second server
+output "machine_type_second" { value = "${google_compute_instance.second.machine_type}" }
+output "name_second" { value = "${google_compute_instance.second.name}" }
+output "instance_id_second" {value = "${google_compute_instance.second.id}" }
